1ace848beaeae24c20c67a6bcb476ae077add8d8
Jay Cincotta <jay.cincotta@gmail.com>
2020-04-12 10:58:58 -0700

    Add stub test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,10 @@
-// import React from 'react';
+import React from "react";
 // import { render } from '@testing-library/react';
 // import App from './App';
 
+test("adds 1 + 2 to equal 3", () => {
+  expect(1 + 2).toBe(3);
+});
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
