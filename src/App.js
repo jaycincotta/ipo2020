29800d0a90f9d7fa9c6d99f385834e874b1a6512
Jay Cincotta <jay.cincotta@gmail.com>
2020-04-26 18:45:52 -0700

    passing cookies

--- src/App.js
+++ src/App.js
@@ -36,11 +36,6 @@ const REPEAT_VISITOR = -1;
 
 export default function App() {
   const [cookies, setCookie, removeCookie] = useCookies(["authID", "authToken"]);
-  setCookie("authID", 4);
-  setCookie(
-    "authToken",
-    "2ab184496bd92a5508ad091ae7e0cca4b1128fa371efaa8ec5ef8e5c16314b38df278b655a887eff77d321d91f7624bb497cfe610e584d6f57d48c78fe3d55e0"
-  );
 
   if (false) {
     // Needed to avoid warnings which kill our CI/CD builds
@@ -138,6 +133,11 @@ export default function App() {
     setFormData(newFormData);
     console.log("GoToThankyou", newFormData);
 
+    setCookie("authID", 4);
+    setCookie(
+      "authToken",
+      "2ab184496bd92a5508ad091ae7e0cca4b1128fa371efaa8ec5ef8e5c16314b38df278b655a887eff77d321d91f7624bb497cfe610e584d6f57d48c78fe3d55e0"
+    );
     setTimeout(() => {
       getStarId(newFormData)
         .then(starId => {
