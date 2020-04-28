68a52fccaf1a2ed446d8ed6c54d67c8dd57f9e90
Jay Cincotta <jay.cincotta@gmail.com>
2020-04-27 17:55:06 -0700

    Remove TESTING WARNING

--- src/AppSettings.js
+++ src/AppSettings.js
@@ -1,5 +1,5 @@
 const isProd = true;
-const version = "Version 0.15 Release Candidate 1";
+const version = "Version 0.16 Release Candidate 2";
 const Localhost = "http://localhost:7071";
 const Prodhost = "https://ipo2020-dev-appservice.azurewebsites.net";
 const FunctionKey = "code=RL9gvBjKUqu7L0AL0OZorm/Zt4Jw4JfJFVvynKN93bjFihZBfkaICw==";
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { PollTestingWarning } from "./PollStatus";
+//import { PollTestingWarning } from "./PollStatus";
 export default function Header({ reset, debug }) {
   return (
     <div id="pageHeader">
@@ -12,7 +12,7 @@ export default function Header({ reset, debug }) {
           Ayuda
         </a>
       </div>
-      <PollTestingWarning onClick={debug} />
+      {/* <PollTestingWarning onClick={debug} /> */}
       <div>
         <img id="headerLogo" src="https://star.ipo.vote/web/images/ipo_logo_notext.png" alt="Logo" onClick={reset} />
         <h1>
